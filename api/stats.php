437f81fe02f7a047cc2df28b8798c669516f0de2
Mu7ammad Khan <117804096+mu7ammadkhan@users.noreply.github.com>
2025-09-22 03:05:31 +0500

    feat(api): stats endpoint (devices_total, rooms_active)

---
+++ api/stats.php
@@ -0,0 +1,9 @@
+﻿<?php
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+// TODO: DB se real counts; abhi placeholders
+echo json_encode([
+  "devices_total" => 24,
+  "rooms_active"  => 7,
+  "ts" => gmdate("c")
+], JSON_UNESCAPED_SLASHES);
